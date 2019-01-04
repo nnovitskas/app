2629cd9acdb6a06d97e194f71fba9f5e6ca285a5
nnovitskas <nadiia.novitskas@bazaarvoice.com>
2019-01-04 12:01:31 +0200

    creating new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { GeneratorComponent } from './components/generator/generator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    GeneratorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/generator/generator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-generator',
+	templateUrl: './generator.component.html',
+	styleUrls: ['./generator.component.sass']
+})
+export class GeneratorComponent {
+
+}
