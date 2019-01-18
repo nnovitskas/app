11ef8d01f530c410415667646aa63237bc30b794
nnovitskas <nadiia.novitskas@bazaarvoice.com>
2019-01-18 16:41:48 +0200

    test examples

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-main',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
 	styleUrls: ['./main.component.sass']
 })
 export class MainComponent {
+  testId = 10;
+  testStatus = 'online';
+  allowNewTest = false;
+  testMakingStatus = 'No test was made!';
+  testName ='';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewTest = true;
+    }, 3000);
+  }
+
+  onMakeTest() {
+    this.testMakingStatus = 'Some new test was made!';
+  }
 
 }
